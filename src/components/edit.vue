72b7f464b14bd9a280e01948f4b3d08e758ea87e
NatsumiSato <natsumi.yamaguchi@noin.tv>
2019-04-26 21:47:12 +0900

    2回目！

--- src/components/edit.vue
+++ src/components/edit.vue
@@ -1,19 +1,66 @@
 <template>
   <div id="editWrap">
     <div class="form-group">
-      <label>アイテム名（クロール生データ）</label>
+      <label>商品名</label>
       <input type="text">
     </div>
     <div class="form-group">
-      <label>アイテム名（クロール生データ）</label>
+      <label>定価</label>
       <input type="text">
     </div>
     <div class="form-group">
-      <label>アイテム名（クロール生データ）</label>
+      <label>販売価格</label>
       <input type="text">
+    </div>
+    <div class="form-group">
+      <label>ブランド</label>
+      <input type="text">
+    </div>
+    <div class="form-group">
+      <label>カテゴリ</label>
+      <input type="text">
+    </div>
+    <div class="form-group">
+      <label>カラー</label>
+      <input type="text">
+    </div>
+    <div class="form-group">
+      <label>容量</label>
+      <input type="text">
+    </div>
+    <div class="form-group">
+      <label>送料</label>
+      <input type="text">
+    </div>
+    <div class="form-group">
+      <label>ポイント</label>
+      <input type="text">
+    </div>
+    <div class="form-group">
+      <label>並行輸入品フラグ</label>
+      <input type="text">
+    </div>
+    <div class="form-group">
+      <label>在庫数</label>
+      <input type="text">
+    </div>
+    <div class="form-group">
+      <label>商品詳細</label>
+      <input type="text">
+    </div>
+    <div class="form-group">
+      <label>成分</label>
+      <input type="text">
+    </div>
+    <div class="form-group">
+      <label>メイン画像</label>
+      <input type="file">
     </div>
   </div>
 </template>
+
+商品画像 N枚(最低1枚, 余裕ができたらN枚)
+
 
 <script>
 export default {
